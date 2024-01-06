e028fcca43e65c1845d71a3cee9ab53daf589121
SHANKAR DAKOLIA <shankardakolia@gmail.com>
2024-01-06 23:05:55 +0530

    Task Three Screens Done...

--- index.js
+++ index.js
@@ -8,5 +8,7 @@ import {name as appName} from './app.json';
 import UserForm from './src/components/UserForm';
 import Contacts from './src/components/Contacts';
 import Screen3 from './src/screens/Screen3';
+import Screen2 from './src/screens/Screen2';
+import Screen1 from './src/screens/Screen1';
 
-AppRegistry.registerComponent(appName, () => Screen3);
+AppRegistry.registerComponent(appName, () => Screen1);
